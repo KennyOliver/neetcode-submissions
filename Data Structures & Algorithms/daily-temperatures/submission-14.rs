a83b572589f0a736a5da099e1868ec996ced539b
Kenny • 餉尓 • ケニー <70860732+KennyOliver@users.noreply.github.com>
2026-07-23 23:30:48 +0200

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/daily-temperatures/submission-14.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut counts = vec![0; temperatures.len()];
+        let mut temps_idx: Vec<usize> = Vec::new(); // monotonic decreasing stack
+
+        for (i, &t) in temperatures.iter().enumerate() {
+            while let Some(&top) = temps_idx.last() {
+                if t > temperatures[top] {
+                    temps_idx.pop();
+                    counts[top] = (i - top) as i32;
+                } else {
+                    break;
+                }
+            }
+
+            temps_idx.push(i);
+        }
+
+        counts
+    }
+}
