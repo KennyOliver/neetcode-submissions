35a38e05f8eeb9db180c2f37a8d1ee7ac9fbdd43
Kenny • 餉尓 • ケニー <70860732+KennyOliver@users.noreply.github.com>
2026-08-07 00:55:22 +0200

    Add: find-minimum-in-rotated-sorted-array - submission-8

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-8.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        // Eliminate left if l > mid
+        // Eliminate right if mid > r
+
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+
+        if nums[l] <= nums[r] {
+            return nums[l];
+        }
+
+        while l < r {
+            let mid = l + (r - l) / 2;
+
+            if nums[mid] > nums[r] {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        nums[l]
+    }
+}
